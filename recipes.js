04785c96ea3824e6ce45d6075bc9cd03f0234c29
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2024-09-12 09:22:30 +0200

    fix: Clear recipe list when attempting to search again
    Refs:l christian-graafschap/SpoonaculairCodeDemo#1

--- recipes.js
+++ recipes.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
         $.ajax({
             url: `https://api.spoonacular.com/recipes/complexSearch?query=${$('#searchValue').val()}&apiKey=c25e2d349ba842ee8186ded1ff30b942`
         }).done(function (data) {
+            const searchResults = document.querySelector('.searchResults');
+            searchResults.innerHTML = "";
             data.results.forEach(recipe => {
-                const searchResults = document.querySelector('.searchResults');
-
                 const newItem = document.createElement('div');
                 newItem.classList.add('recipe');
                 newItem.innerHTML = `<h3>${recipe.title}</h3><a href="ingredients.html?id=${recipe.id}"><img src=${recipe.image} /></a>`;
